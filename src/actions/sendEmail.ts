9b1523ec844c986168f7f8702af5716dc6352736
Ivan Horbynko <horbynko.i@gmail.com>
2023-10-21 11:05:24 +0300

    create util getErrorMessage

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Resend } from "resend";
+import { getErrorMessage, index } from "@/src/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const testEmail = process.env.TEST_EMAIL;
@@ -9,12 +10,30 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  console.log(senderEmail);
-  console.log(message);
-  await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: [testEmail as string],
-    subject: "Message from contact form",
-    text: "Hello world",
-  });
+  //simple serverside validation
+  if (!index(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email!",
+    };
+  }
+
+  if (!index(message, 5000)) {
+    return {
+      error: "Invalid sender email!",
+    };
+  }
+
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: [testEmail as string],
+      subject: "Message from contact form",
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return { error: getErrorMessage(error) };
+  }
+
+  return data;
 };
