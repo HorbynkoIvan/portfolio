611e321452df7fc072c11846cb8df80967138919
HorbynkoIvan <horbynko.i@gmail.com>
2023-11-18 22:09:24 +0200

    background color combination

--- src/components/BackgroundElements.tsx
+++ src/components/BackgroundElements.tsx
@@ -1,11 +1,11 @@
 export const BackgroundElements = () => (
   <>
     <div
-      className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem]
-         w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+      className="bg-[#FC766AFF ] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem]
+    w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#0B3954FF ]"></div>
     <div
-      className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem]
-         w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
-         xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+      className="bg-[#5B84B1FF] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem]
+    w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
+    xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#17202A ]"></div>
   </>
 );
